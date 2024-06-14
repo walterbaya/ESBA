6f695b8a027e9e4a37d4bdf2a63643570e206fc7
walterbaya <56239454+walterbaya@users.noreply.github.com>
2024-06-14 12:35:04 -0300

    Se finaliza ejercicio 3

--- Segundo Cuatrimestre/Herramientas de Programacion/Excepciones/Excepciones-archivos/Ejercicio3.py
+++ Segundo Cuatrimestre/Herramientas de Programacion/Excepciones/Excepciones-archivos/Ejercicio3.py
@@ -3,35 +3,48 @@ dicc_archivo = {}
 
 try:
 
-    #Abrimos en modo solo lectura
+    #Apertura del archivo
+
     archivo = open("archivos/listado-comunidades-indigenas.csv", "r", encoding="UTF-8")
     contenido = archivo.read()
     filas = contenido.split("\n")
     
-    #procesarlo y obtener cuantas comunidades 
-    #se registran por provincia, cada registro corresponde a una
-    #comunidad. Tenga precaución en la elección del separador ya que la coma también
-    #está incluida dentro de uno de los campos.
+    #Procesamiento
     
+    #Obtengo el encabezado
     encabezado = filas[0][2 : len(filas[0]) - 1].split('","')
+
+    #Filtramos las filas vacias
+    filas.remove("")
     
+    #Elimino el encabezado
+    filas = filas[1:]
     
+    #Cada fila representa una comunidad
     for fila in filas:
+
+        #Obtengo para cada fila, la provincia en la que esta registrada
         comunidad_provincia = fila[2 : len(fila) - 1].split('","')[encabezado.index('comunidad_provincia')]
-        
-        #Me fijo si la provincia esta definida ya.
+            
         if dicc_archivo.get(comunidad_provincia) == None:
             dicc_archivo[comunidad_provincia] = 1
         
-        #Si esta definida le agrego una comunidad, porque en esta fila figura esa provincia otra vez.
         else:    
             dicc_archivo[comunidad_provincia] = dicc_archivo[comunidad_provincia] + 1 
-        
+
+    print(dicc_archivo)
+            
     archivo.close()
 
 except FileNotFoundError:
     print("No existe el archivo")
 except UnicodeDecodeError:
     print("Hubo un error, no se pudo decodificar un byte en una posición")
+except  Exception as e:
+    print("Error,", e)
+
+
+
+
 
     
