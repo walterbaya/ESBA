b64b391f154041ce9045fceed39255f79cc46aa0
walterbaya <56239454+walterbaya@users.noreply.github.com>
2024-09-17 10:46:35 -0300

    Se agrega ejercicio 1 practica 5

---
+++ Tercer Cuatrimestre/Tecnicas Avanzadas de Programacion/ArraysTarea/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace WalterBaya
+{
+    class Program
+    {
+        static void arraySum(int[] array)
+        {
+            int acum = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                acum += array[i];
+            }
+
+            Console.WriteLine("Sum: " + acum);
+        }
+        static void biggest(int[] array)
+        {
+            int biggest = array[0];
+
+
+            for (int i = 1; i < 10; i++)
+            {
+                if (array[i] >= biggest)
+                {
+                    biggest = array[i];
+                }
+            }
+
+            Console.WriteLine("The biggest element is: " + biggest);
+        }
+
+        static void smallest(int[] array)
+        {
+            int smallest = array[0];
+
+
+            for (int i = 1; i < 10; i++)
+            {
+                if (array[i] <= smallest)
+                {
+                    smallest = array[i];
+                }
+            }
+
+            Console.WriteLine("The smallest element is: " + smallest);
+        }
+
+        static void printArray(int[] array)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Array[" + i + "] = " + array[i]);
+            }
+        }
+
+        static int[] load_array()
+        {
+            int[] array = new int[10];
+            //Cargar el arreglo
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Ingrese el elemento " + (i + 1) + " del arreglo: ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            return array;
+
+        }
+
+        static void Main(string[] args)
+        {
+            //Ejercicio 1
+
+            int[] array = load_array();
+
+            printArray(array);
+
+            arraySum(array);
+
+            biggest(array);
+
+            smallest(array);
+        }
+    }
+}
