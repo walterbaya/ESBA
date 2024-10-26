7291f2d64aacddb2763a7b0162c41aab4b538f6b
walterbaya <56239454+walterbaya@users.noreply.github.com>
2024-10-26 14:03:09 -0300

    Se sube ejercicio 2 clase

---
+++ Tercer Cuatrimestre/Tecnicas Avanzadas de Programacion/Ejercicio2-clase.cs
@@ -0,0 +1,30 @@
+/*2) Diseñar las siguientes clases que representan una factura.
+
+CHeader  (encabezado).
+
+-	string m_nFactura
+-	DateTime  m_fechaEmision
+-	string m_RazonSocial
+-	string m_Domicilio
+-	string m_Cuit
+-	string m_IngresosBrutos
+-	string m_Tipo
+-	DateTime m_fechaIncioActividad
+-	string m_dni.
+
+CDetail (detalle)
+-	long m_codProducto
+-	string m_descripcion
+-	int m_cantidad
+-	double m_precioUnitario
+
+CFooter (pie página)
+	
+-	double m_subTotal
+-	double m_total
+-	double m_iva
+-	double m_descuento
+
+Todos los datos miembros son privados, por lo tanto escribir todas la funciones de acceso apropiadas para cada campo, teniendo en cuenta validaciones por lo menos básicas.
+Ojo que hay campos que NO SE DEBEN modificar con funciones de acceso.
+*/
